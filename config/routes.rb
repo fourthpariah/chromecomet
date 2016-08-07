312f441f6c7b30af44d977fd39f6ab26f92ddc61
fourthpariah <fourthpariah@gmail.com>
2016-08-07 06:01:29 +0000

    built out navigation and added welcome controller, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+
   resources :invoices
+  
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
