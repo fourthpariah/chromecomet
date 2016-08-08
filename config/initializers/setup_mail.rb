9861152d78778c5be50a3dc018ec85dfa3fd3032
fourthpariah <fourthpariah@gmail.com>
2016-08-08 04:08:28 +0000

    set up heroku config vars

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address             => 'smtp.sendgrid.net',
   :port                => '587',
   :authentication      => :plain,
-  :user_name           => 'app54776580@heroku.com',
-  :password            => '4y605spk5450',
+  :user_name           => ENV['SENDGRID_USERNAME'],
+  :password            => ENV['SENDGRID_PASSWORD'],
   :domain              => 'heroku.com',
   :enable_starttls_auto => true
 }
