55dcfaea9b12448abf6114f4cf4d3acd28723685
fourthpariah <fourthpariah@gmail.com>
2016-08-09 04:33:34 +0000

    added dropdown element selection for invoices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809024414) do
+ActiveRecord::Schema.define(version: 20160809042247) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160809024414) do
     t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status_type"
   end
 
   create_table "users", force: :cascade do |t|
