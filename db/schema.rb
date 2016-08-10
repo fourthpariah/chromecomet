d541087124581528d7955e2a6f1c320d610aa1be
fourthpariah <fourthpariah@gmail.com>
2016-08-10 02:24:15 +0000

    added employee id db form call

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809042247) do
+ActiveRecord::Schema.define(version: 20160810020814) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -22,14 +22,22 @@ ActiveRecord::Schema.define(version: 20160809042247) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "job_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
     t.string   "company"
     t.decimal  "tax"
-    t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status_type"
+    t.integer  "employee_id"
   end
 
   create_table "users", force: :cascade do |t|
